56e52de2c724fc1ef3476f049892c7fdfef50e19
Gustavo Lopes <gustavo_lopes_12@hotmail.com>
2017-08-03 22:38:17 -0300

    Commit inicial

---
+++ src/models/Autenticacao.java
@@ -0,0 +1,13 @@
+package models;
+
+/**
+ * Created by gudominguete on 03/08/17.
+ */
+public interface Autenticacao {
+
+    public void autenticacao();
+
+    public void recuperarSenha();
+
+    public void trocarSenha();
+}
