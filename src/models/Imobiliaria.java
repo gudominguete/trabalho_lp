32211e71d7dcaca3cae9b1b13d3676a09fb2c0ec
Gustavo Lopes <gustavo_lopes_12@hotmail.com>
2017-08-06 10:59:57 -0300

    Modelagem imobiliaria

--- src/models/Imobiliaria.java
+++ src/models/Imobiliaria.java
@@ -2,15 +2,73 @@ package models;
 
 import java.util.List;
 
-/**
- * Created by gudominguete on 05/08/17.
- */
 public class Imobiliaria extends Usuario {
 
     private String cnpj;
     private List<Imovel> listaImoveis;
 
+    /**
+     * Construtor do objeto
+     * @param nome
+     * @param login
+     * @param senha
+     */
     public Imobiliaria(String nome, String login, String senha) {
         super(nome, login, senha);
     }
+
+    /**
+     * Método para retornar o CNPJ do objeto
+     * @return
+     */
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    /**
+     * Método para alterar o CNPJ do objeto
+     * @param cnpj
+     */
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    /**
+     * Método para buscar a lista de imoveis do objeto
+     * @return
+     */
+    public List<Imovel> getListaImoveis() {
+        return listaImoveis;
+    }
+
+    /**
+     * Método para inserir um imovel na imobiliaria
+     * @param imovel
+     */
+    public void inserirImovel(Imovel imovel){
+        this.listaImoveis.add(imovel);
+    }
+
+    /**
+     * Método para remover um imovel da imobiliaria
+     * @param idImovel
+     * @return
+     */
+    public Imovel removerImovel(Integer idImovel){
+
+        Imovel imovelRemovido;
+
+        for(int cont =0; cont < this.listaImoveis.size(); cont++){
+
+            if(listaImoveis.get(cont).getId().equals(idImovel)){
+                imovelRemovido = listaImoveis.get(cont);
+                this.listaImoveis.remove(cont);
+                return imovelRemovido;
+            }
+        }
+
+        return null;
+    }
+
+
 }
--- src/models/Imovel.java
+++ src/models/Imovel.java
@@ -4,4 +4,14 @@ package models;
  * Created by gudominguete on 05/08/17.
  */
 public class Imovel {
+
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
